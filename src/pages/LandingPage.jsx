6546726dc6f7bb522882b981371bf62f744a7b6e
miguelmanabo4 <miguelmanabo4@gmail.com>
2025-03-24 04:05:30 +0000

    Implementation: Added Text Responsiveness

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -28,25 +28,25 @@ export default function LandingPage() {
                     <div className="h-[88vh]">
                         <div className="flex items-center flex-col justify-evenly h-[100%]">
 
-                            <div className="font-Poppins text-white text-[85px] font-bold text-center">
+                            <div className="font-Poppins text-white text-[7.3vw] font-bold text-center ">
                                 Free <span className="text-[#E9E62E]">Lunch</span> Eligibility <br />System
                             </div>
 
-                            <div className="flex justfiy-center w-[82vw] text-center text-white font-Poppins text-[20px]">
+                            <div className="flex justfiy-center w-[82vw] text-center text-white font-Poppins text-[1.5vw] ">
                                 Designed to streamline the delivery of nutritious meals to students during their term in La Verdad Christian College. The system aims to improve student health and engagement by providing a reliable and user-friendly platform for meal access within the school environment.
                             </div>
 
-                            <div className="flex justfiy-center text-center text-white font-Poppins text-[20px] font-bold">
+                            <div className="flex justfiy-center text-center text-white font-Poppins text-[1.4vw] font-bold ">
                                 you can now proceed as:
                             </div>
 
-                            <div className="flex gap-[4vw]">
+                            <div className="flex gap-[4vw] ">
                                 <Link to="admin_login">
-                                    <button className="w-[150px] h-[40px] bg-[#05305D] rounded-[8px] font-Inter text-white font-bold text-[15]">
+                                    <button className="w-[150px] h-[40px] bg-[#05305D] rounded-[8px] font-Inter text-white font-bold text-[1.1vw]">
                                         ADMIN
                                     </button>
                                 </Link>
-                                <button className="w-[150px] h-[40px] bg-[#0F5FC2] rounded-[8px] font-Inter text-white font-bold text-[15]">
+                                <button className="w-[150px] h-[40px] bg-[#0F5FC2] rounded-[8px] font-Inter text-white font-bold text-[1.1vw]">
                                     KITCHEN STAFF
                                 </button>
                             </div>
--- src/pages/admin/AdminLogin.jsx
+++ src/pages/admin/AdminLogin.jsx
@@ -5,41 +5,44 @@ import { Link } from "react-router-dom"
 export default function AdminLogin() {
   return (
     <>
-      <div className="bg-[#F8FAFB] w-screen h-screen border-black border-[1px]">
+      <div className="bg-[#F8FAFB] w-screen h-screen border-black border-[1px] overflow-y-hidden">
         <div className="grid grid-cols-2 grid-rows-1 h-[100vh]">
-          <div>
+          <div className="overflow-y-hidden">
             <div className="h-[60px]">
               <Link to="/">
                 <MoveLeft className="m-[15px]" />
               </Link>
             </div>
-            <div className="flex justify-center items-center h-[90vh]">
-              <img src="/Login_Illustration_1.svg" alt="login illustration" />
+            <div className="flex justify-center items-center h-[90vh] overflow-y-hidden">
+              <img src="/Login_Illustration_1.svg" alt="login illustration" className="overflow-y-hidden"/>
             </div>
           </div>
           <div className="w-full h-full">
             <div className="flex justify-center items-center h-full">
-              <div className="bg-[#ffffff] w-[44vw] h-[90vh]">
+
+              <div className="bg-[#ffffff] w-[44vw] max-w-[70%] h-[90vh] overflow-y-hidden">
                 <div className="h-[100%]">
                   <div className="h-[100%] mx-[2vw]">
                     <div className="h-[20%]">
                       <div className="flex justify-center items-start flex-col h-[100%]">
-                        <span className="font-Poppins text-[40px]">Hello there! <br /></span>
-                        <span className="font-Poppins text-[12px] mt-[-5px]">Please enter your credentials to log-in your account. </span>
+                        <span className="font-Poppins text-[2.7vw]">Hello there! <br /></span>
+                        <span className="font-Poppins text-[1.1vw] mt-[-5px]">Please enter your credentials to log-in your account. </span>
                       </div>
                     </div>
                     <div className="h-[40%]">
                       <div className="h-[100%] flex flex-col items-start justify-evenly">
                         <div className="w-[100%]">
-                          <span className="font-Poppins text-[14px]">username</span>
+                          <span className="font-Poppins text-[1.1vw]">username</span>
                           <input type="text" className="pl-[5px] rounded-[s] w-[100%] h-[6vh] border-[1px]" />
                         </div>
                         <div className="w-[100%]">
-                          <span className="font-Poppins text-[14px]">password</span>
+                          <span className="font-Poppins text-[1.1vw]">password</span>
                           <input type="password" className="pl-[5px] rounded-[s] w-[100%] h-[6vh] border-[1px]" />
                         </div>
                         <div className="w-[100%] flex justify-center items-center">
-                          Forgot <span className="text-[#0F5FC2]"> &nbsp;password?</span>
+                          <p className="font-Poppings text-[1.1vw]">
+                            Forgot <span className="text-[#0F5FC2]"> &nbsp;password?</span>
+                          </p>
                         </div>
                       </div>
                     </div>
@@ -47,7 +50,7 @@ export default function AdminLogin() {
                       <div className="h-[100%] flex flex-col justify-evenly ">
                         <div>
                           <button className="w-[100%] bg-[#0F5FC2] h-[5.5vh] rounded-[5px]">
-                            <span className="font-Poppins text-[16px] text-white">
+                            <span className="font-Poppins text-[1.3vw] text-white">
                               Log in
                             </span>
                           </button>
@@ -63,8 +66,8 @@ export default function AdminLogin() {
                         <div>
                           <button className="w-[100%] bg-[#1F3463] h-[5.5vh] rounded-[5px]">
                             <div class="flex justify-center items-center gap-[8px]">
-                              <FcGoogle className="size-[25px]" />
-                              <span className="font-Poppins text-[16px] text-white">
+                              <FcGoogle className="size-[1.5vw]" />
+                              <span className="font-Poppins text-[1.3vw] text-white">
                                 Log in with google
                               </span>
                             </div>
